ba6f0547e6fce5ddc6b4872802a173ed7136a00f
Jake Soenneker <jake@soenneker.com>
2025-03-28 07:46:25 -0500

    Fixes build

--- src/MsTeamsReceptor.cs
+++ src/MsTeamsReceptor.cs
@@ -24,7 +24,7 @@ public sealed class MsTeamsReceptor : ServiceBusReceptor, IMsTeamsReceptor
     {
     }
 
-    public override ValueTask OnMessageReceived(string messageContent, Type? type)
+    public override ValueTask OnMessageReceived(string messageContent, Type? type, CancellationToken cancellationToken = default)
     {
         try
         {
